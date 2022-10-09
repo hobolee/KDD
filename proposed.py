ba2af4c09301d95ba2c10a7e32ff85fb951adde7
CinderD <zchendf@connect.ust.hk>
2022-10-09 23:22:54 +0800

    stageOne code from Kentaro

---
+++ proposed.py
@@ -0,0 +1,12 @@
+# Here we are going to write our proposals. 
+
+# new way of aggregation from the neighbourhood of the original instance
+def aggregation(orig_neighs,....)
+    dist_prot = []
+    return dist_prot
+
+
+# new way of retireval method of neighourhoods. 
+def retrieve_neighbors(testx, instance_prototypes, idx_current_nodes....)
+    orig_neighs = []
+    return orig_neighs
